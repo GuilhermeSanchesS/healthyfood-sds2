1b828af04625b668e7d5ffa997219e16a41d8dd1
G8U17 <guilhermebr18@hotmail.com>
2021-01-04 22:15:00 -0300

    Project created

---
+++ backend/src/main/java/com/devsanches/healthyfood/HealthyfoodApplication.java
@@ -0,0 +1,13 @@
+package com.devsanches.healthyfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyfoodApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsanches/healthyfood/HealthyfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsanches.healthyfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
